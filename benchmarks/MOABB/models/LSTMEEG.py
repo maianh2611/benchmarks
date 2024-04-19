c1f53aabdfdb8ca308a7460b5eff20a4cafa5dab
Thi Mai Anh Nguyen <maianh.26111997@gmail.com>
2024-04-19 02:07:21 -0400

    LSTMEEG

--- benchmarks/MOABB/models/LSTMEEG.py
+++ benchmarks/MOABB/models/LSTMEEG.py
@@ -1,10 +1,3 @@
-"""EEGNet from https://doi.org/10.1088/1741-2552/aace8c.
-Shallow and lightweight convolutional neural network proposed for a general decoding of single-trial EEG signals.
-It was proposed for P300, error-related negativity, motor execution, motor imagery decoding.
-
-Authors
- * Davide Borra, 2021
-"""
 import torch
 import speechbrain as sb
 
@@ -43,13 +36,16 @@ class LSTMEEG(torch.nn.Module):
     def __init__(
         self,
         input_shape=None,  # (1, T, C, 1)
-        cnn_temporal_kernels=8,
-        cnn_temporal_kernelsize=(33, 1),
-        cnn_pool_type="avg",
+        conv0_kernels = 8,
+        conv0_size = 3,
+        conv1_kernels = 32,
+        conv1_size = 5,
+        conv2_kernels = 64,
+        conv2_size = 7,
         dropout=0.5,
         dense_max_norm=0.25,
-        dense_n_neurons=4,
-        activation_type="elu",
+        num_neurons = 32,
+        activation_type="relu",
     ):
         super().__init__()
         if input_shape is None:
@@ -67,88 +63,146 @@ class LSTMEEG(torch.nn.Module):
         else:
             raise ValueError("Wrong hidden activation function")
         self.default_sf = 128  # sampling rate of the original publication (Hz)
-        # T = input_shape[1]
-        C = input_shape[2]
 
-        # CONVOLUTIONAL MODULE
-        self.conv_module = torch.nn.Sequential()
-        # Temporal convolution
-        self.conv_module.add_module(
+
+        self.conv_module_0 = torch.nn.Sequential()
+        self.conv_module_0.add_module(
             "conv_0",
             sb.nnet.CNN.Conv2d(
                 in_channels=1,
-                out_channels=cnn_temporal_kernels,
-                kernel_size=cnn_temporal_kernelsize,
+                out_channels=conv0_kernels,
+                kernel_size=conv0_size,
                 padding="same",
                 padding_mode="constant",
                 bias=False,
                 swap=True,
             ),
         )
-        self.conv_module.add_module(
+        self.conv_module_0.add_module(
             "bnorm_0",
             sb.nnet.normalization.BatchNorm2d(
-                input_size=cnn_temporal_kernels, momentum=0.01, affine=True,
+                input_size=conv0_kernels, momentum=0.01, affine=True,
             ),
         ),
-        self.conv_module.add_module(
+        self.conv_module_0.add_module(
             "activation",
             activation,
         )
-        self.conv_module.add_module(
+        self.conv_module_0.add_module(
             "pool_1",
             sb.nnet.pooling.Pooling2d(
-                pool_type=cnn_pool_type,
+                pool_type='max',
                 kernel_size=[2,2],
                 pool_axis=[1, 2],
             ),
         )
-        self.conv_module.add_module("dropout_1", torch.nn.Dropout(p=dropout))
+
+        self.conv_module_1 = torch.nn.Sequential()
+        self.conv_module_1.add_module(
+            "conv_0",
+            sb.nnet.CNN.Conv2d(
+                in_channels=conv0_kernels,
+                out_channels=conv1_kernels,
+                kernel_size=conv1_size,
+                padding="same",
+                padding_mode="constant",
+                bias=False,
+                swap=True,
+            ),
+        )
+        self.conv_module_1.add_module(
+            "bnorm_0",
+            sb.nnet.normalization.BatchNorm2d(
+                input_size=conv1_kernels, momentum=0.01, affine=True,
+            ),
+        ),
+        self.conv_module_1.add_module(
+            "activation",
+            activation,
+        )
+        self.conv_module_1.add_module(
+            "pool_1",
+            sb.nnet.pooling.Pooling2d(
+                pool_type='max',
+                kernel_size=[2,2],
+                pool_axis=[1, 2],
+            ),
+        )
+        self.conv_module_2 = torch.nn.Sequential()
+        self.conv_module_2.add_module(
+            "conv_0",
+            sb.nnet.CNN.Conv2d(
+                in_channels=conv1_kernels,
+                out_channels=conv2_kernels,
+                kernel_size=conv2_size,
+                padding="same",
+                padding_mode="constant",
+                bias=False,
+                swap=True,
+            ),
+        )
+        self.conv_module_2.add_module(
+            "bnorm_0",
+            sb.nnet.normalization.BatchNorm2d(
+                input_size=conv2_kernels, momentum=0.01, affine=True,
+            ),
+        ),
+        self.conv_module_2.add_module(
+            "activation",
+            activation,
+        )
+        # self.conv_module_2.add_module(
+        #     "pool_1",
+        #     sb.nnet.pooling.Pooling2d(
+        #         pool_type='max',
+        #         kernel_size=[2,2],
+        #         pool_axis=[1, 2],
+        #     ),
+        # )
+        self.conv_module_2.add_module("dropout_1", torch.nn.Dropout(p=dropout))
+
+       
+
         ones_tensor = torch.ones((1,) + tuple(input_shape[1:-1]) + (1,))
+        convOut = self.conv_module_0(ones_tensor)
+        convOut = self.conv_module_1(convOut)
+        convOut = self.conv_module_2(convOut)
+
+        self.lstm = sb.nnet.RNN.LSTM(
+            input_shape= convOut.shape,
+            hidden_size=num_neurons,
+            num_layers=1,
+        )
+        outLSTM,_ =  self.lstm(convOut)
 
 
         # Shape of intermediate feature maps
-        out = self.conv_module(
-            torch.ones((1,) + tuple(input_shape[1:-1]) + (1,))
-        )
 
-        self.lstm = sb.nnet.RNN.LSTM(
-            input_shape= out.shape,
-            hidden_size=64,
-            num_layers=1,
-        )
-
+        dense_input_size = self._num_flat_features(outLSTM)
+        
         self.lstm_dropout = torch.nn.Dropout(p=dropout)
 
-        self.lstm2 = sb.nnet.RNN.LSTM(
-            input_shape= out.shape,
-            hidden_size=32,
-            num_layers=1,
-        )
-
-        self.lstm_dropout = torch.nn.Dropout(p=dropout)
 
         self.dense_module = torch.nn.Sequential()
         self.dense_module.add_module(
-            "flatten", torch.nn.Flatten(),
+            "flatten", torch.nn.Flatten(1),
         )
-
         self.dense_module.add_module(
-            "fc_out",
+            "fc_out_1",
             sb.nnet.linear.Linear(
-                input_size=out.shape[1]*32,
-                n_neurons=dense_n_neurons,
+                input_size=dense_input_size,
+                n_neurons=4,
                 max_norm=dense_max_norm,
             ),
         )
         self.dense_module.add_module("act_out", torch.nn.LogSoftmax(dim=1))
-        '''
-        dense_input_size = self._num_flat_features(out)
-        # DENSE MODULE
-        
-        '''
 
-    def _num_flat_features(self, x):
+    def getDenseShape(self,input):
+        x = input
+        x = self.conv_module(x)
+        return x
+        
+    def _num_flat_features(self, x, startDim = 1):
         """Returns the number of flattened features from a tensor.
 
         Arguments
@@ -157,7 +211,7 @@ class LSTMEEG(torch.nn.Module):
             Input feature map.
         """
 
-        size = x.size()[1:]  # all dimensions except the batch dimension
+        size = x.size()[startDim:]  # all dimensions except the batch dimension
         num_features = 1
         for s in size:
             num_features *= s
@@ -171,10 +225,11 @@ class LSTMEEG(torch.nn.Module):
         x : torch.Tensor (batch, time, EEG channel, channel)
             Input to convolve. 4d tensors are expected.
         """
-        x = self.conv_module(x)
+        x = self.conv_module_0(x)
+        x = self.conv_module_1(x)
+        x = self.conv_module_2(x)
         out, _=  self.lstm(x)
+        
         x = self.lstm_dropout(out)
-        out2, _ = self.lstm2(x)
-        x = self.lstm_dropout(out2)
         x = self.dense_module(x)
         return x
